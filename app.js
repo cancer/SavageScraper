68125bd06f212d8cf0fe70f61ce84bad403c67cc
cancer <zodiac.cancer.j6@gmail.com>
2015-02-15 06:36:28 +0900

    Add static files

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 
 var express = require('express');
 var app = module.exports =  express();
+app.use(express.static(__dirname + '/static'));
 
 var sites = {
   forestUnion: require('./modules/forest-union')
 };
-
-app.get('/', function(req, res){
-  res.send('Welcome to SavageScraper!!');
-});
 
 app.get('/forest-union', require('./modules/forest-union'));
 app.get('/donpachi', require('./modules/donpachi'));
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+React.render(
+  React.createElement('h1', null, "Welcome to SavageScraper!!"),
+  document.getElementById('app')
+)
