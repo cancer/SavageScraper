fbf49326f568f8b79a9be09d4e0f1649112e1d53
cancer <zodiac.cancer.j6@gmail.com>
2015-02-15 14:13:01 +0900

    Want to use ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
 var gulp = require('gulp');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var chalk = require('chalk');
 var server = require('gulp-express');
+var service = null;
 
 gulp.task('exec', function(){
-  exec('node app.js', function(error, stdout, stderr){
-    console.log(stdout);
-    console.log(chalk.red(stderr));
+  if(service) {
+    service.kill('SIGKILL');
+    service = undefined;
+    process.removeListener('exit', onProcessExit);
+  }
+  service = spawn('node', ['--harmony', 'app.js']);
+  service.stdout.setEncoding('utf8');
+  service.stderr.setEncoding('utf8');
+  service.stdout.on('data', log);
+  service.stderr.on('data', log);
+  service.on('exit', function(code, sig){
+    console.log(chalk.blue('Service process exited with code', code, sig));
   });
-});
+  process.on('exit', onProcessExit);
 
-gulp.task('server', function(){
-  server.run({
-    file: 'app.js'
-  });
+  function log(data){
+    console.log(data.trim());
+  }
+
+  function onProcessExit(){
+    service.kill();
+  }
 });
 
 gulp.task('watch', function(){
-  gulp.watch('**/*.js', server.run);
+  gulp.watch('app.js', ['exec']);
+  gulp.watch('modules/**/*.js', ['exec']);
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['exec', 'watch']);
