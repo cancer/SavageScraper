d9696ef76d42c4377b67e83c70c84fc8593b7ef0
cancer <zodiac.cancer.j6@gmail.com>
2015-02-15 14:42:28 +0900

    Add Union traditional parser

--- modules/api/forest_union.js
+++ modules/api/forest_union.js
@@ -1,44 +1,11 @@
 'use strict';
 
-var lib = require('../_lib');
+var parseUnionTraditional = require('../parser/union_traditional');
 
 module.exports = function(request, response){
-  var weekEndIndexes = {
-    0: [0, 6],
-    7: [5, 6]
-  };
-
-  lib.client.fetch('http://www.musa-web.net/union/index.cfm?ID=2', {
-    normalizeWhitespace: true
-  }, function(err, $, res){
-    var sundayIndex = 0;
-    var weekEndIndex = weekEndIndexes[sundayIndex];
-    var dayOrNight = ['Day', 'Night'];
-    var results = [];
-    $('.dataTable').find('tr').each(function(idx){
-      if(idx === 0) return; // 見出し行
-
-      var $cols = $(this).find('td');
-      results.push(weekEndIndex.reduce(function(memo, val){
-        var cellIdx = val + 1;
-        var dayOfMonth = ((idx - 1) * 7) + cellIdx;
-        var date = lib.moment().date(dayOfMonth);
-
-        if(lib.moment().isAfter(date)) return memo; // 過去の情報は含めない
-
-        var cols = [];
-        $cols.eq(val + 1).find('p > span').each(function(i){
-          var bookable = !$(this).hasClass('forest') && !$(this).hasClass('reserved');
-          cols.push({
-            date: date.format('MM/DD dddd '),
-            period: dayOrNight[i],
-            bookable: bookable
-          });
-        });
-        memo.push(cols);
-        return memo;
-      }, []));
-    });
-    response.send(lib._.flatten(results));
+  parseUnionTraditional('2').then((result) => {
+    response.send(result);
+  }).catch((error) => {
+    response.send(error);
   });
 };
---
+++ modules/parser/union_traditional.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var lib = require('../_lib');
+var fetch = require('../fetch');
+
+module.exports = function(id){
+  var options = {
+    normalizeWhitespace: true
+  };
+  var deferred = Promise.defer();
+
+  fetch('http://www.musa-web.net/union/index.cfm?ID=2', options).then(($, res) => {
+    var weekEndIndex = [0, 6];
+    var dayOrNight = ['Day', 'Night'];
+    var results = [];
+    $('.dataTable').find('tr').each(function(idx){
+      if(idx === 0) return; // 見出し行
+
+      var $cols = $(this).find('td');
+      results.push(weekEndIndex.reduce(function(memo, val){
+        var cellIdx = val + 1;
+        var dayOfMonth = ((idx - 1) * 7) + cellIdx;
+        var date = lib.moment().date(dayOfMonth);
+
+        if(lib.moment().isAfter(date)) return memo; // 過去の情報は含めない
+
+        var cols = [];
+        $cols.eq(val + 1).find('p > span').each(function(i){
+          var bookable = !$(this).hasClass('forest') && !$(this).hasClass('reserved');
+          cols.push({
+            date: date.format('MM/DD dddd '),
+            period: dayOrNight[i],
+            bookable: bookable
+          });
+        });
+        memo.push(cols);
+        return memo;
+      }, []));
+    });
+
+    deferred.resolve(lib._.flatten(results));
+  }).catch((err) => {
+    console.log(err);
+    deferred.reject({
+      error: err.msg,
+      content: '非公開'
+    })
+  });
+
+  return deferred.promise;
+};
+
