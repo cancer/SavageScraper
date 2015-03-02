a7072e96ed69db10acbc919d4ef05a5688addacb
geckotang <macky1207@gmail.com>
2015-03-02 20:57:50 +0900

    パース部分作成

--- modules/api/raid.js
+++ modules/api/raid.js
@@ -16,7 +16,7 @@ module.exports = function(request, response){
   lib.client.fetch(url, options).then((result) => {
 
     var $ = result.$;
-    var $table = $('.calendar');
+    var $tables = $('.calendar');
     var result = {
       all: [],
       holiday: []
@@ -24,8 +24,43 @@ module.exports = function(request, response){
     var idx_sunday = 0;
     var idx_saturday = 6;
 
-    $table.each(function(idx, v){
+    //曜日行を削除する
+    $tables.find('tr:first-child').remove();
 
+    $tables.each((idx, table) => {
+      var $calendar = $(table);
+      var $table = $calendar.find('.calendarborder');
+      var $rows = $table.find('tr');
+      var title = $calendar.find('caption').text().trim();//2015年 3月
+      var parseTitle = title.match(/(\d+)年\s?(\d+)月/);
+      var year  = (+parseTitle[1]);
+      var month = (+parseTitle[2])-1;
+
+      if (request.params.year === year && request.params.month === month) {
+
+        $rows.each((idx, row) => {
+          var $row = $(row);
+          var $cols = $row.find('td');
+          $cols.each((idx, col) => {
+            var $col = $(col);
+            var day = +$col.find('b').text().trim();
+            $col.find('b').remove();
+            $col.find('.holname').remove();
+            var item = {
+              date: lib.moment([ year, month, day ]).format('MM/DD dddd'),
+              summary: $col.text(),
+              bookable: _isBookable($col.text())
+            };
+            if (day) {
+              result.all.push(item);
+              if (idx === idx_sunday || idx === idx_saturday) {
+                result.holiday.push(item)
+              }
+            }
+          });
+        });
+
+      }
     });
 
     response.send(result);
@@ -34,5 +69,27 @@ module.exports = function(request, response){
       content: 'error'
     });
   });
+
+  function _isBookable (detail) {
+    var bookable = {
+      day: true,
+      night:true 
+    };
+    if ( /貸切受付NG/.test(detail) ) {
+      bookable.day = false;
+      bookable.night = false;
+    }
+    if ( /BBQ定例会/.test(detail) ) {
+      bookable.day = false;
+      bookable.night = false;
+    }
+    if (/貸切昼/.test(detail)) {
+      bookable.day = false;
+    }
+    if (/貸切夜/.test(detail)) {
+      bookable.night = false;
+    }
+    return bookable;
+  };
 
 };
