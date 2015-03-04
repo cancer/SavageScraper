0737011b136c5e37b9d85c84cabbb537c0a3c805
geckotang <macky1207@gmail.com>
2015-03-04 21:32:03 +0900

    特区追加

--- modules/api/tokku.js
+++ modules/api/tokku.js
@@ -9,7 +9,7 @@ module.exports = function(request, response){
 
   var year = request.params.year;
   var month = request.params.month;
-  var formattedDate = lib.moment([ year, month-1 ]).format('YYYY-MM')
+  var formattedDate = lib.moment([ year, month ]).format('YYYY-MM');
   var url = 'http://www.tokku-switch.jp/wp/%E4%BA%88%E7%B4%84%E3%83%BB%E4%BA%88%E7%B4%84%E7%8A%B6%E6%B3%81?cid=all&yr='+request.params.year+'&format=calendar&time=month&dy=1&month='+(request.params.month+1);
   var options = {
     normalizeWhitespace: true,
@@ -39,8 +39,20 @@ module.exports = function(request, response){
         var $cols = $row.find('td');
         $cols.each((idx, col) => {
           var $col = $(col);
-          var col_id = $col.attr('id');
-
+          var day, summary, item;
+          //前月、次月のセルは無視する
+          if (!$col.hasClass('nextmonth')) {
+            day = +$col.find('.mc-date').text().trim();
+            item = {
+              date: lib.moment([ year, month, day ]).format('MM/DD dddd'),
+              summary: $col.find('.event-title.summary').text(),
+              bookable: _isBookable($col)
+            };
+            result.all.push(item);
+            if (idx === idx_sunday || idx === idx_saturday) {
+              result.holiday.push(item)
+            }
+          }
         });
       });
 
@@ -53,11 +65,34 @@ module.exports = function(request, response){
     });
   });
 
-  function _isBookable (detail) {
+  function _isBookable ($col) {
     var bookable = {
       day: true,
       night: true
     };
+    //昼貸切、休日定例会
+    if (
+      ($col.find('img[src$="/star.png"][style$="#ffffff;"]').length > 0)
+      || ($col.find('img[src$="/games.png"][style$="#;"]').length > 0)
+    ) {
+      bookable.day = false;
+    }
+    //夜貸切、ナイター定例
+    if (
+      ($col.find('img[src$="/star.gif"][style$="#000000;"]').length > 0)
+      || ($col.find('img[src$="/games.png"][style$="#000000;"]').length > 0)
+    ) {
+      bookable.night = false;
+    }
+    //定休日、フィールドメンテナンス、撮影
+    if (
+      ($col.find('img[src$="/appointment.png"]').length > 0)
+      || ($col.find('img[src$="/shop.png"]').length > 0)
+      || ($col.find('img[src$="/movies.png"]').length > 0)
+    ) {
+      bookable.day = false;
+      bookable.night = false;
+    }
     return bookable;
   };
 
