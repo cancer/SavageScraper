8c7a91f96888edc36afe274444adf8e0cae27c40
geckotang <macky1207@gmail.com>
2015-03-05 02:31:13 +0900

    Village2追加

--- app/script/modules/api/village2.js
+++ app/script/modules/api/village2.js
@@ -19,27 +19,62 @@ module.exports = function(request, response){
 
     var $ = result.$;
     var $tables = $('.hpb-ca-tb1');
-    var result = {
+    var results = {
       all: [],
       holiday: []
     };
-    var idx_sunday = 0;
-    var idx_saturday = 6;
+    var key_sunday = 'Sun';
+    var key_saturday = 'Sat';
+    var _prev_month;
 
     $tables.each((idx, table) => {
       var $table = $(table);
+      var $trs = $table.find('tr');
+      var title = $table.find('.hpb-subh02-cell1').text() || '';
+      var parseTitle = title.match(/(\d+)年\s*(\d+)?月/);
+      var _year  = (parseInt(parseTitle[1], 10));
+      var _month = (parseInt(parseTitle[2], 10)-1) || _prev_month+1;
+      _prev_month = _month;
+
+      if (request.params.year === _year && request.params.month === _month) {
+        //0番目のtrががタイトル行、1番目のtr見出し行なのでスルー
+        for (var i=2, l=$trs.length; i<l; ++i) {
+          var $tr = $trs.eq(i);
+          var $tds = $tr.find('td');
+          //0番目のtdが日付
+          var parseDate = $tds.eq(0).text().trim().match(/\d+\/(\d+).*/);
+          var day = parseInt(parseDate[1], 10);
+          //1番目のtdが曜日
+          var dayOfWeek = $tds.eq(1).text().trim();
+          //2番目のtdが内容
+          var detail = $tds.eq(2).text().trim();
+          var item = {
+            date: lib.moment([ _year, _month, day ]).format('MM/DD dddd'),
+            summary: detail,
+            bookable: _isBookable(detail)
+          };
+            results.all.push(item);
+            if (dayOfWeek === key_sunday || dayOfWeek === key_saturday) {
+              results.holiday.push(item)
+            }
+        }
+      }
+
     });
 
-    response.send(result);
+    response.send(results);
   }).catch((err) => {
     response.send({
       content: 'error'
     });
   });
 
-  function _isBookable ($col) {
-    var bookable = true;
-    return bookable;
+  function _isBookable (detail) {
+    if (detail === '' || detail === '空き'){
+      return true;
+    } else {
+      return false;
+    }
   };
 
 };
