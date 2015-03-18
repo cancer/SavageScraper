0f1ec54ef1f6f9f005338e4328812f49d012c45c
geckotang <macky1207@gmail.com>
2015-03-18 21:07:09 +0900

    パーサの変更に合わせてアカウント名だけを渡すように変更

--- app/script/modules/api/donpachi.js
+++ app/script/modules/api/donpachi.js
@@ -3,7 +3,7 @@
 var parseGoogleCalendar = require('../parser/google_calendar');
 
 module.exports = function(request, response){
-  parseGoogleCalendar('https://www.google.com/calendar/feeds/gisvgsbqur4j1rjn890nh3kfeo@group.calendar.google.com/public/basic?alt=json').then(function(result){
+  parseGoogleCalendar('gisvgsbqur4j1rjn890nh3kfeo@group.calendar.google.com').then(function(result){
     response.send(result);
   }).catch(function(err){
     response.send(err)
--- app/script/modules/api/rockhill.js
+++ app/script/modules/api/rockhill.js
@@ -3,7 +3,7 @@
 var parseGoogleCalendar = require('../parser/google_calendar');
 
 module.exports = function(request, response){
-  parseGoogleCalendar('https://www.google.com/calendar/feeds/uqgnng6jo9438d0eo6609lms2g@group.calendar.google.com/public/basic?alt=json').then(function(result){
+  parseGoogleCalendar('uqgnng6jo9438d0eo6609lms2g@group.calendar.google.com').then(function(result){
     response.send(result);
   }).catch(function(err){
     response.send(err)
