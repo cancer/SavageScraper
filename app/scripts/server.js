b375c3f01d94432ca06db5ffebaab3400318180f
cancer <zodiac.cancer.j6@gmail.com>
2015-03-23 14:28:47 +0900

    Change API paths

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -24,38 +24,38 @@ app.get('/scripts/bundle.js', function(req, res) {
 //
 
 // Union traditional
-app.get('/desert_union/:year/:month', require('./modules/api/desert_union'));
-app.get('/forest_union/:year/:month', require('./modules/api/forest_union'));
-app.get('/u_box/:year/:month',        require('./modules/api/u_box'));
-app.get('/union_base/:year/:month',   require('./modules/api/union_base'));
+app.get('/api/desert_union/:year/:month', require('./modules/api/desert_union'));
+app.get('/api/forest_union/:year/:month', require('./modules/api/forest_union'));
+app.get('/api/u_box/:year/:month',        require('./modules/api/u_box'));
+app.get('/api/union_base/:year/:month',   require('./modules/api/union_base'));
 
-app.get('/battlecity/:year/:month',   require('./modules/api/battlecity'));
+app.get('/api/battlecity/:year/:month',   require('./modules/api/battlecity'));
 
 // Asobiba
-app.get('/asobiba_itabashi_1f/:year/:month', require('./modules/api/asobiba_itabashi_1f'));
-app.get('/asobiba_itabashi_b1/:year/:month', require('./modules/api/asobiba_itabashi_b1'));
-app.get('/asobiba_yokohama/:year/:month', require('./modules/api/asobiba_yokohama'));
-app.get('/asobiba_akiba/:year/:month', require('./modules/api/asobiba_akiba'));
+app.get('/api/asobiba_itabashi_1f/:year/:month', require('./modules/api/asobiba_itabashi_1f'));
+app.get('/api/asobiba_itabashi_b1/:year/:month', require('./modules/api/asobiba_itabashi_b1'));
+app.get('/api/asobiba_yokohama/:year/:month', require('./modules/api/asobiba_yokohama'));
+app.get('/api/asobiba_akiba/:year/:month', require('./modules/api/asobiba_akiba'));
 
-app.get('/cimax/:year/:month', require('./modules/api/cimax'));
+app.get('/api/cimax/:year/:month', require('./modules/api/cimax'));
 
-app.get('/raid/:year/:month', require('./modules/api/raid'));
+app.get('/api/raid/:year/:month', require('./modules/api/raid'));
 
-app.get('/no9/:year/:month', require('./modules/api/no9'));
+app.get('/api/no9/:year/:month', require('./modules/api/no9'));
 
-app.get('/tokku/:year/:month', require('./modules/api/tokku'));
+app.get('/api/tokku/:year/:month', require('./modules/api/tokku'));
 
-app.get('/village2/:year/:month', require('./modules/api/village2'));
+app.get('/api/village2/:year/:month', require('./modules/api/village2'));
 
 // Google Calendar
-app.get('/donpachi',     require('./modules/api/donpachi'));
-app.get('/rockhill',     require('./modules/api/rockhill'));
-app.get('/qoolong',      require('./modules/api/qoolong'));
-app.get('/whitebase',    require('./modules/api/whitebase'));
-app.get('/trigger_talk', require('./modules/api/trigger_talk'));
-app.get('/code7', require('./modules/api/code7'));
-app.get('/estate', require('./modules/api/estate'));
-app.get('/infini', require('./modules/api/infini'));
+app.get('/api/donpachi',     require('./modules/api/donpachi'));
+app.get('/api/rockhill',     require('./modules/api/rockhill'));
+app.get('/api/qoolong',      require('./modules/api/qoolong'));
+app.get('/api/whitebase',    require('./modules/api/whitebase'));
+app.get('/api/trigger_talk', require('./modules/api/trigger_talk'));
+app.get('/api/code7', require('./modules/api/code7'));
+app.get('/api/estate', require('./modules/api/estate'));
+app.get('/api/infini', require('./modules/api/infini'));
 
 app.listen(3000);
 console.log('SavageScraper started on port 3000');
